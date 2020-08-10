a6ad65420a97c84e91e24bdc8264a5dc46eebf43
Julia <yulyakonoval1998@gmail.com>
2020-08-10 20:18:53 +0300

    js chooseOperator applied

--- script.js
+++ script.js
@@ -1,28 +1,84 @@
-const title = document.createElement("h1");
-title.id = "title";
-title.textContent = "Calculator";
-document.body.appendChild(title);
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.previousOperandTextElement = previousOperandTextElement;
+    // clear();
+  }
 
-const calc = document.createElement("div");
-calc.id = "calc";
-document.body.appendChild(calc);
+  // clear() {
+  //   this.currentOperand() = "";
+  //   this.previousOperand() = "";
+  //   this.operation = undefined;
+  // }
+  // delete() {}
+  appendNumber(number) {
+    this.currentOperand = this.currentOperand.toString() + number.toString;
+  }
 
-const screen = document.createElement("div");
-screen.id = "screen";
-calc.appendChild(screen);
-
-const buttonContainer = document.createElement("div");
-buttonContainer.id = "button-container";
-calc.appendChild(buttonContainer);
-
-function createGrid(rows, cols) {
-  buttonContainer.style.setProperty("--grid-rows", rows);
-  buttonContainer.style.setProperty("--grid-cols", cols);
-
-  for (let i = 0; i < rows * cols; i++) {
-    let numberBtn = document.createElement("button");
-    buttonContainer.appendChild(numberBtn).className = "number-btn";
+  chooseOperator(operationBtn) {
+    if (operationBtn === "+") {
+      function sumAll(...nums) {
+        let sum = 0;
+        for (num of nums) sum += arg;
+        return sum;
+      }
+    } else if (operationBtn === "-") {
+      function subtractAll(...nums) {
+        let subtract = 0;
+        for (num of nums) subtract -= arg;
+        return subtract;
+      }
+    } else if (operationBtn === "*") {
+      function multiplyAll(...nums) {
+        let multiply = 0;
+        for (num of nums) multiply *= arg;
+        return multiply;
+      }
+    } else if (operationBtn === "/") {
+      function divideAll(...nums) {
+        let divide = 0;
+        for (num of nums) divide /= arg;
+        return divide;
+      }
+    }
   }
 }
 
-createGrid(4, 4);
+// updateDisplay() {
+//   this.currentOperandTextElement.innerText = this.currentOperand;
+// }
+
+const numberBtn = document.querySelectorAll("[data-number]");
+const screen = document.querySelector("#screen");
+const operationBtn = document.querySelectorAll("[data-operator]");
+const equalsBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const allClearBtn = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+numberBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperator(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+operationBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperator(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+// operationBtn.forEach((button)=>{
+//   button.addEventListener("click", () => {
+
+//   }
